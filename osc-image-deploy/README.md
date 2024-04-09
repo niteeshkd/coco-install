538f971c4851722d9a2e92111724e14f4744c456
Pradipta Banerjee <pradipta.banerjee@gmail.com>
2024-04-09 16:44:20 +0530

    Image based deployment via OSC operator
    Signed-off-by: Pradipta Banerjee <pradipta.banerjee@gmail.com>

---
+++ osc-image-deploy/README.md
@@ -0,0 +1,45 @@
+# Setup
+
+You can run the all-in-one setup script `./install.sh` or follow
+the individual steps described below.
+
+## Install the OSC operator
+
+```sh
+oc apply -f my_catalog.yaml
+oc apply -f ns.yaml
+oc apply -f og.yaml
+oc apply -f subs.yaml
+```
+
+You should see controller-manager pods in the `openshift-sandboxed-containers-operator` namespace
+
+```sh
+oc get pods -n openshift-sandboxed-containers-operator
+```
+
+### Enable Image based deployment
+
+- Enable featuregate
+
+```sh
+oc apply -f osc-fg-cm.yaml
+```
+
+### Create KataConfig
+
+The example KataConfig deploys Kata on all the worker nodes.
+You can use the KataConfigPoolSelector to install Kata only on specific nodes.
+
+```sh
+oc apply -f kataconfig.yaml
+```
+
+This will start the install and you can watch the progress by observing the
+status of the `kata-oc` MachineConfigPool
+
+```sh
+oc get mcp
+```
+
+After successful install `kata` and `kata-cc` runtimeclass will be created
---
+++ osc-image-deploy/install.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+# Function to check if the oc command is available
+function check_oc() {
+    if ! command -v oc &>/dev/null; then
+        echo "oc command not found. Please install the oc CLI tool."
+        exit 1
+    fi
+}
+
+# Function to wait for the operator deployment object to be ready
+function wait_for_deployment() {
+    local deployment=$1
+    local namespace=$2
+    local timeout=300
+    local interval=5
+    local elapsed=0
+    local ready=0
+
+    while [ $elapsed -lt $timeout ]; do
+        ready=$(oc get deployment -n "$namespace" "$deployment" -o jsonpath='{.status.readyReplicas}')
+        if [ "$ready" == "1" ]; then
+            echo "Operator $deployment is ready"
+            return 0
+        fi
+        sleep $interval
+        elapsed=$((elapsed + interval))
+    done
+    echo "Operator $deployment is not ready after $timeout seconds"
+    return 1
+}
+
+# Function to wait for service endpoints IP to be available
+# Example json for service endpoints. IP is available in the "addresses" field
+#  "subsets": [
+#    {
+#        "addresses": [
+#            {
+#                "ip": "10.135.0.25",
+#                "nodeName": "coco-worker-1.testocp.local",
+#                "targetRef": {
+#                    "kind": "Pod",
+#                    "name": "controller-manager-87ffb6bfd-5zzvf",
+#                    "namespace": "openshift-sandboxed-containers-operator",
+#                    "uid": "00059394-29fb-44bf-8121-d1df02524ea8"
+#                }
+#            }
+#        ],
+#        "ports": [
+#            {
+#                "port": 443,
+#                "protocol": "TCP"
+#            }
+#        ]
+#    }
+#]
+
+function wait_for_service_ep_ip() {
+    local service=$1
+    local namespace=$2
+    local timeout=300
+    local interval=5
+    local elapsed=0
+    local ip=0
+
+    while [ $elapsed -lt $timeout ]; do
+        ip=$(oc get endpoints -n "$namespace" "$service" -o jsonpath='{.subsets[0].addresses[0].ip}')
+        if [ -n "$ip" ]; then
+            echo "Service $service IP is available"
+            return 0
+        fi
+        sleep $interval
+        elapsed=$((elapsed + interval))
+    done
+    echo "Service $service IP is not available after $timeout seconds"
+    return 1
+}
+
+# Function to wait for MachineConfigPool (MCP) to be ready
+function wait_for_mcp() {
+    local mcp=$1
+    local timeout=300
+    local interval=5
+    local elapsed=0
+    local ready=0
+    while [ $elapsed -lt $timeout ]; do
+        if [ "$statusUpdated" == "True" ] && [ "$statusUpdating" == "False" ] && [ "$statusDegraded" == "False" ]; then
+            echo "MCP $mcp is ready"
+            return 0
+        fi
+        sleep $interval
+        elapsed=$((elapsed + interval))
+        statusUpdated=$(oc get mcp "$mcp" -o=jsonpath='{.status.conditions[?(@.type=="Updated")].status}')
+        statusUpdating=$(oc get mcp "$mcp" -o=jsonpath='{.status.conditions[?(@.type=="Updating")].status}')
+        statusDegraded=$(oc get mcp "$mcp" -o=jsonpath='{.status.conditions[?(@.type=="Degraded")].status}')
+    done
+
+}
+
+# Function to wait for runtimeclass to be ready
+function wait_for_runtimeclass() {
+
+    local runtimeclass=$1
+    local timeout=300
+    local interval=5
+    local elapsed=0
+    local ready=0
+
+    # oc get runtimeclass "$runtimeclass" -o jsonpath={.metadata.name} should return the runtimeclass
+    while [ $elapsed -lt $timeout ]; do
+        ready=$(oc get runtimeclass "$runtimeclass" -o jsonpath='{.metadata.name}')
+        if [ "$ready" == "$runtimeclass" ]; then
+            echo "Runtimeclass $runtimeclass is ready"
+            return 0
+        fi
+        sleep $interval
+        elapsed=$((elapsed + interval))
+    done
+
+    echo "Runtimeclass $runtimeclass is not ready after $timeout seconds"
+    return 1
+}
+
+# Function to apply the operator manifests
+function apply_operator_manifests() {
+    # Apply the manifests, error exit if any of them fail
+    oc apply -f ns.yaml || exit 1
+    oc apply -f my_catalog.yaml || exit 1
+    oc apply -f og.yaml || exit 1
+    oc apply -f subs.yaml || exit 1
+}
+
+# Check if oc command is available
+check_oc
+
+# Apply the operator manifests
+apply_operator_manifests
+
+wait_for_deployment controller-manager openshift-sandboxed-containers-operator || exit 1
+
+# Create the feature gate configmap
+oc apply -f osc-fg-cm.yaml || exit 1
+
+# Wait for the service endpoints IP to be available
+wait_for_service_ep_ip webhook-service openshift-sandboxed-containers-operator || exit 1
+
+# Create Kataconfig
+oc apply -f kataconfig.yaml || exit 1
+
+# Wait for sometime before checking for MCP
+sleep 10
+
+wait_for_mcp kata-oc || exit 1
+
+# Wait for runtimeclass kata to be ready
+wait_for_runtimeclass kata || exit 1
+
+# Wait for runtimeclass kata-cc to be ready
+wait_for_runtimeclass kata-cc || exit 1
+
+echo "Sandboxed containers operator with CoCo support is installed successfully"
